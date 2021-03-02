9c01416907bb8d3baebf7d35a5dfb6bb0b68316a
Ben Sparkes <ben-sparkes@pineapple-studios.co.uk>
2021-03-02 17:11:18 +0000

    Added PlayerprefsX and a gamemanager skeleton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance { get; private set; }
+
+    public bool bPlaying;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (Instance != null)
+            Destroy(Instance);
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+        Application.targetFrameRate = 60;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
