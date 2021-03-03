806924b779af0d2ccf7833e0f97ca9eb97caf53c
MarcAndrews-PineappleStudios <marc-andrews@pineapple-studios.co.uk>
2021-03-03 13:01:21 +0000

    Movement & flipping Version 1 - Added The player movement to be a constant velocity update - Added a temporary flipping system to change player gravity to PlayerMovement - Added a temporary test scene to test movement changes & to show how it works

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,80 @@
+﻿using System.Security;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D playerRb;
+    [SerializeField]
+    private int speed=4, jumpForce=300;
+    [SerializeField]
+    private float gravity;
+    [SerializeField]
+    private bool flipped = false, flip = false, isGrounded = true;
+
+    void Awake() 
+    {      
+        playerRb = gameObject.GetComponent<Rigidbody2D>();      
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && isGrounded)
+        {            
+            Jump(flipped);
+        }
+        //temp grav changer for testing. Will inverse gravity on player
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            flip = true;
+        }
+
+        if(flip)
+        {
+            GravityChange(flipped);
+        }        
+    }
+
+    void FixedUpdate()
+    {
+        //Moves the player forward at a constant speed
+        playerRb.velocity = new Vector2(speed,playerRb.velocity.y);
+    }
+
+    //grounded check
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    //Applies a jump force depending on if the player is flipped or not
+    void Jump(bool isFlipped)
+    {
+        if(!isFlipped)
+            playerRb.AddForce(new Vector2(0,(jumpForce)), ForceMode2D.Force);
+        else   
+            playerRb.AddForce(new Vector2(0,(-jumpForce)), ForceMode2D.Force);
+        isGrounded = false;
+    }
+
+    //Changes the players gravity when they are flipped
+    void GravityChange(bool isFlipped)
+    {
+        if(!flipped)
+        {
+            playerRb.gravityScale = -1;
+            flipped = true;
+        }
+        else
+        {
+            playerRb.gravityScale = 1;
+            flipped = false;
+        }
+        flip = false;        
+    }
+}
