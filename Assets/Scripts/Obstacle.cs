2f194cb132e54b428b6f500b5d99dd92b1593de2
MarcAndrews-PineappleStudios <marc-andrews@pineapple-studios.co.uk>
2021-03-04 14:11:05 +0000

    Created an Obstacle script with an enum for multiple types - Added movement and rotation for sawblades

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] 
+    private ObstacleType obstacleType;
+    [SerializeField]
+    private float sawMoveDistance = -4f, sawMoveTime = 0.6f, sawSpinAmount = 720f, sawSpinTime = 0.6f;
+
+    void Start()
+    {
+        if(obstacleType == ObstacleType.SAW_BLADE)
+        {
+            LeanTween.moveX(gameObject, (transform.position.x + sawMoveDistance), sawMoveTime).setLoopPingPong();
+            LeanTween.rotateAround(gameObject, Vector3.forward, sawSpinAmount, sawMoveTime).setLoopClamp();
+        }
+    }
+}
+
+public enum ObstacleType
+{
+    NONE,
+    SAW_BLADE,
+    SPIKE,
+    DESTRUCTION_RIFT
+}
