256aa1978f90e49af0ea748a5153dd1d3f33854c
Ben Sparkes <ben-sparkes@pineapple-studios.co.uk>
2021-03-04 21:59:20 +0000

    Parallax background added. Still needs tweaking

---
+++ Assets/Scripts/BackgroundParralax.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParralax : MonoBehaviour
+{
+    private float length, startpos;
+    public float ParallaxEffect;
+
+
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate()
+    {
+
+        float temp = (Camera.main.transform.position.x * (1 - ParallaxEffect));
+        float distance = (Camera.main.transform.position.x * ParallaxEffect);
+
+        transform.position = new Vector3(startpos + distance, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
+    }
+
+
+}
